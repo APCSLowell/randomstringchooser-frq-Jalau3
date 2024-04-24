dba7287c53fc24233ae0df20596ca410e43da2e2
Jalau3 <143014960+Jalau3@users.noreply.github.com>
2024-04-24 00:44:39 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -5,7 +5,7 @@ public class RandomStringChooser
   private ArrayList <String> arr;
   RandomStringChooser(String[] a){
     arr = new ArrayList <String> ();
-    for(String b: arr)
+    for(String b: a)
       arr.add(b);
   }
 
