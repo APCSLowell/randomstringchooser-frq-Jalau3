b722b5796b8d1b8f598756972ca59d84eb3ef637
Jalau3 <143014960+Jalau3@users.noreply.github.com>
2024-04-24 00:37:23 -0700

    Update RandomLetterChooser.java

--- src/main/java/RandomLetterChooser.java
+++ src/main/java/RandomLetterChooser.java
@@ -4,9 +4,9 @@ public class RandomLetterChooser extends RandomStringChooser
 	public RandomLetterChooser(String str)
 	{ 
 		/* to be implemented in part (b) */
-		array = new ArrayList <String> ();
-		for(int i = 0; i<str.length(); i++)
-			array.add(str.substring(i, i+1));
+		arr2 = new ArrayList <String> ();
+		for(String str:this.getSingleLetters(str))
+			arr2.add(str);
 		
 	}
 	
